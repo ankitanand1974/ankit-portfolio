f703864c298b2a9ece1a312f2488f331823691f1
Ankit <ankitanand1974@gmail.com>
2024-08-12 22:17:18 +0530

    added intersection observer

--- src/app/components/Skills.js
+++ src/app/components/Skills.js
@@ -1,5 +1,5 @@
 "use client";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 
 const skills = [
   { name: "JavaScript", icon: "icons/javascript.svg", level: 90 },
@@ -12,25 +12,44 @@ const skills = [
   { name: "Git", icon: "/icons/git.png", level: 85 },
 ];
 
+
 export default function Skills() {
   const [animate, setAnimate] = useState(false);
+  const skillsRef = useRef(null);
 
   useEffect(() => {
-    const timer = setTimeout(() => {
-      setAnimate(true);
-    }, 100); // delay to ensure the component is rendered before starting the animation
-    return () => clearTimeout(timer);
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        if (entry.isIntersecting) {
+          setAnimate(true);
+        }
+      },
+      {
+        root: null, // use the viewport
+        rootMargin: '0px',
+        threshold: 0.1, // trigger when 10% of the section is visible
+      }
+    );
+
+    if (skillsRef.current) {
+      observer.observe(skillsRef.current);
+    }
+
+    return () => {
+      if (skillsRef.current) {
+        observer.unobserve(skillsRef.current);
+      }
+    };
   }, []);
 
   return (
     <section
       id="skills"
+      ref={skillsRef}
       className="py-20 bg-gray-50 dark:bg-gray-800 transition-colors duration-300"
     >
       <div className="container mx-auto px-6">
-        <h2 className="text-3xl font-semibold text-center mb-12 dark:text-white">
-          My Skills
-        </h2>
+        <h2 className="text-3xl font-semibold text-center mb-12 dark:text-white">My Skills</h2>
         <div className="grid grid-cols-2 md:grid-cols-4 gap-6">
           {skills.map((skill, index) => (
             <div
@@ -48,7 +67,7 @@ export default function Skills() {
               <div className="w-full bg-gray-200 rounded-full h-1 dark:bg-gray-600 mt-4">
                 <div
                   className={`bg-yellow-600 h-1 rounded-full dark:bg-yellow-400 transition-width duration-2000 ease-in-out`}
-                  style={{ width: animate ? `${skill.level}%` : "0%" }}
+                  style={{ width: animate ? `${skill.level}%` : '0%' }}
                 ></div>
               </div>
               <p className="text-sm dark:text-gray-300 mt-2">{skill.level}%</p>
