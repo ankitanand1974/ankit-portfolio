631f2941c2f93bef94cdc79f8e460e9392378e37
Ankit <ankitanand1974@gmail.com>
2024-08-16 14:36:48 +0530

    added flip ui

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,4 +1,6 @@
+import { FlipWords } from "./ui/flip-words";
 export default function Hero() {
+  const words = ["Full Stack Web Developer", "Software Developer"];
   return (
     <div className="bg-gradient-to-t from-blue-500 to-purple-600 text-white dark:from-black dark:to-indigo-900">
       <div className="container mx-auto px-6 py-24 text-center flex justify-evenly ">
@@ -6,7 +8,9 @@ export default function Hero() {
 
         <div className="flex-col justify-center py-20">
           <h1 className="text-5xl font-bold mb-5">Ankit Anand</h1>
-          <p className="text-2xl mb-9">Full Stack Web Developer</p>
+          <div className="text-2xl mb-9">
+            <FlipWords words={words} /> <br />
+          </div>
           <a
             href="#contact"
             className="bg-white text-blue-500 dark:bg-gray-800 dark:text-blue-300 px-6 py-3 rounded-full font-semibold hover:bg-blue-100 dark:hover:bg-gray-700 transition duration-300"
