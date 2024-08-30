1c815c1fbf5595a54cf4c84bbcaedffb1b36bcab
Ankit <ankitanand1974@gmail.com>
2024-08-30 16:38:52 +0530

    fix

--- src/components/TimelineSection.js
+++ src/components/TimelineSection.js
@@ -17,11 +17,11 @@ const TimelineSection = () => {
       ),
       content: (
         <div>
-          <ul className="text-neutral-800 dark:text-neutral-200 font-normal mb-8 list-disc text-justify text-base">
+          <ul className="text-neutral-800 dark:text-neutral-200 leading-8 font-normal mb-8 list-disc text-justify text-base lg:text-lg lg:leading-9">
             <li>
               Developed a learner tracking application with React, integrating
               data visualization for engagement monitoring, resulting in
-              improved user engagement metrics.{" "}
+              improved user engagement metrics.
             </li>
             <li>
               Built backend services with Golang, implemented RESTful APIs,
@@ -36,72 +36,30 @@ const TimelineSection = () => {
             </li>
             <li>
               Documented the application with Swagger, and ensured
-              well-optimized and structured code reducing debugging time by 60%
+              well-optimized and structured code reducing debugging time by 60%.
             </li>
           </ul>
           
-          <div className="gap-4 text-lg">
-            <h>Technologies used</h>
-            <div className="grid p-1">
-              <p className="shadow-[inset_0_0_0_2px_#616467] text-black px-2 py-1 rounded-full tracking-widest font-bold bg-transparent hover:bg-[#616467] hover:text-white dark:text-neutral-200 transition duration-200">ReactJS</p>
-              <p className="shadow-[inset_0_0_0_2px_#616467] text-black px-2 py-1 rounded-full tracking-widest font-bold bg-transparent hover:bg-[#616467] hover:text-white dark:text-neutral-200 transition duration-200">NodeJS</p>
-              <p className="shadow-[inset_0_0_0_2px_#616467] text-black px-2 py-1 rounded-full tracking-widest font-bold bg-transparent hover:bg-[#616467] hover:text-white dark:text-neutral-200 transition duration-200">GoLang</p>
-              <p className="shadow-[inset_0_0_0_2px_#616467] text-black px-2 py-1 rounded-full tracking-widest font-bold bg-transparent hover:bg-[#616467] hover:text-white dark:text-neutral-200 transition duration-200">Redis</p>
-              <p className="shadow-[inset_0_0_0_2px_#616467] text-black px-2 py-1 rounded-full tracking-widest font-bold bg-transparent hover:bg-[#616467] hover:text-white dark:text-neutral-200 transition duration-200">Docker</p>
-              <p className="shadow-[inset_0_0_0_2px_#616467] text-black px-2 py-1 rounded-full tracking-widest font-bold bg-transparent hover:bg-[#616467] hover:text-white dark:text-neutral-200 transition duration-200">ScyllaDB</p>
+          <div className="text-lg">
+            <h>Technologies used : </h>
+            <div className="grid grid-cols-3 lg:grid-cols-5 text-[80%] md:text-[100%] gap-[2%] gap-y-4 py-4">
+              <p className="shadow-[inset_0_0_0_2px_#616467] text-center text-black px-2 py-1 rounded-full tracking-widest font-bold bg-transparent dark:text-neutral-200 transition duration-200">ReactJS</p>
+              <p className="shadow-[inset_0_0_0_2px_#616467] text-center text-black px-2 py-1 rounded-full tracking-widest font-bold bg-transparent dark:text-neutral-200 transition duration-200">NodeJS</p>
+              <p className="shadow-[inset_0_0_0_2px_#616467] text-center text-black px-2 py-1 rounded-full tracking-widest font-bold bg-transparent dark:text-neutral-200 transition duration-200">GoLang</p>
+              <p className="shadow-[inset_0_0_0_2px_#616467] text-center text-black px-2 py-1 rounded-full tracking-widest font-bold bg-transparent dark:text-neutral-200 transition duration-200">Redis</p>
+              <p className="shadow-[inset_0_0_0_2px_#616467] text-center text-black px-2 py-1 rounded-full tracking-widest font-bold bg-transparent dark:text-neutral-200 transition duration-200">Docker</p>
+              <p className="shadow-[inset_0_0_0_2px_#616467] text-center text-black px-2 py-1 rounded-full tracking-widest font-bold bg-transparent dark:text-neutral-200 transition duration-200">ScyllaDB</p>
             </div>
           </div>
         </div>
       ),
     },
-    {
-      title: "Early 2023",
-      content: (
-        <div>
-          <p className="text-neutral-800 dark:text-neutral-200 text-xs md:text-sm font-normal mb-8">
-            I usually run out of copy, but when I see content this big, I try to
-            integrate lorem ipsum.
-          </p>
-          <p className="text-neutral-800 dark:text-neutral-200 text-xs md:text-sm font-normal mb-8">
-            Lorem ipsum is for people who are too lazy to write copy. But we are
-            not. Here are some more examples of beautiful designs I built.
-          </p>
-          <div className="grid grid-cols-2 gap-4"></div>
-        </div>
-      ),
-    },
-    {
-      title: "Changelog",
-      content: (
-        <div>
-          <p className="text-neutral-800 dark:text-neutral-200 text-xs md:text-sm font-normal mb-4">
-            Deployed 5 new components on Aceternity today
-          </p>
-          <div className="mb-8">
-            <div className="flex gap-2 items-center text-neutral-700 dark:text-neutral-300 text-xs md:text-sm">
-              ✅ Card grid component
-            </div>
-            <div className="flex gap-2 items-center text-neutral-700 dark:text-neutral-300 text-xs md:text-sm">
-              ✅ Startup template Aceternity
-            </div>
-            <div className="flex gap-2 items-center text-neutral-700 dark:text-neutral-300 text-xs md:text-sm">
-              ✅ Random file upload lol
-            </div>
-            <div className="flex gap-2 items-center text-neutral-700 dark:text-neutral-300 text-xs md:text-sm">
-              ✅ Himesh Reshammiya Music CD
-            </div>
-            <div className="flex gap-2 items-center text-neutral-700 dark:text-neutral-300 text-xs md:text-sm">
-              ✅ Salman Bhai Fan Club registrations open
-            </div>
-          </div>
-          <div className="grid grid-cols-2 gap-4"></div>
-        </div>
-      ),
-    },
+  
   ];
 
   return (
     <div className="w-full">
+    <h>Experience</h>
       <Timeline data={data} />
     </div>
   );
